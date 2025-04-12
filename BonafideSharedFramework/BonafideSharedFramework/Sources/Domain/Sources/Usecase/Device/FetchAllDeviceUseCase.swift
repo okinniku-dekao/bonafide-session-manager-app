e55167ddcc5cc8ea9dd96cdacfbd49fe02cf7d65
hideto1198 <hideto1198@yahoo.co.jp>
2025-04-12 23:51:38 +0900

    リネーム

---
+++ BonafideSharedFramework/BonafideSharedFramework/Sources/Domain/Sources/Usecase/Device/FetchAllDeviceUseCase.swift
@@ -0,0 +1,11 @@
+//
+//  FetchAllDeviceUseCase.swift
+//  Domain
+//
+//  Created by 東　秀斗 on 2025/04/12.
+//
+
+
+public protocol FetchAllDeviceUseCase {
+    func callAsFunction(for store: Store?) async throws -> [Device]
+}
