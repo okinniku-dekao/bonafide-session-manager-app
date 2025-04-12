87007322bc0d3fc1d1f2e50d4006050cd2058cbe
hideto1198 <hideto1198@yahoo.co.jp>
2025-04-13 00:20:31 +0900

    SessionUseCase追加

---
+++ BonafideSharedFramework/BonafideSharedFramework/Sources/Domain/Sources/Usecase/Session/FetchAllSessionUseCase.swift
@@ -0,0 +1,11 @@
+//
+//  FetchAllSessionUseCase.swift
+//  Domain
+//
+//  Created by 東　秀斗 on 2025/04/13.
+//
+
+
+public protocol FetchAllSessionUseCase {
+    func callAsFunction(userId: String) async throws -> [Session]
+}
