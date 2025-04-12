9a7024ad67404b12cce10ed319faf669af5d8d78
hideto1198 <hideto1198@yahoo.co.jp>
2025-04-13 01:46:55 +0900

    pathを指定

--- BonafideSharedFramework/BonafideSharedFramework/Sources/Usecase/Package.swift
+++ BonafideSharedFramework/BonafideSharedFramework/Sources/Usecase/Package.swift
@@ -19,7 +19,8 @@ let package = Package(
             name: "Usecase",
             dependencies: [
                 "Domain"
-            ]
+            ],
+            path: "Sources"
         ),
         .testTarget(
             name: "UsecaseTests",
