3d100810f41a5b8ab6fee2927c97dcc8a2c85de5
hideto1198 <hideto1198@yahoo.co.jp>
2025-04-16 10:59:54 +0900

    AsyncStreamを処理できるeffect追加

--- BonafideSharedFramework/Sources/PresentationHelper/Sources/Effect+Extension.swift
+++ BonafideSharedFramework/Sources/PresentationHelper/Sources/Effect+Extension.swift
@@ -8,5 +8,17 @@
 import ComposableArchitecture
 
 public extension Effect {
-    
+    static func stream<T: Equatable & Sendable>(
+        _ stream: AsyncThrowingStream<T, any Error>,
+        map action: @escaping @Sendable (T) -> Action,
+        catch handler: (@Sendable (_ error: any Error, _ send: Send<Action>) async -> Void)? = nil
+    ) -> Effect {
+        .run { [action = action] send in
+            for try await value in stream {
+                await send(action(value))
+            }
+        } catch: {
+            await handler?($0, $1)
+        }
+    }
 }
