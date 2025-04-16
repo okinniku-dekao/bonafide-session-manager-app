3f8384f166d9168e68e787578ff53414d0a19904
hideto1198 <hideto1198@yahoo.co.jp>
2025-04-16 16:29:59 +0900

    端末IDが保存されていなかったら登録画面に遷移させる

--- RegisterApp/RegisterApp/Sources/Presentation/AppRootFeature/Sources/Feature/AppRootFeature.swift
+++ RegisterApp/RegisterApp/Sources/Presentation/AppRootFeature/Sources/Feature/AppRootFeature.swift
@@ -5,22 +5,36 @@
 //  Created by hideto.higashi on 2025/03/04.
 //
 
+import Foundation
 import ComposableArchitecture
 import Composition
 import Domain
 import PresentationHelper
+import MainTabFeature
+import RegisterDeviceFeature
 
 @Reducer
 public struct AppRootFeature: Sendable {
     public init() {}
 
-    public struct State: Equatable {
-        public init() {
-        }
+    @Reducer
+    public enum Destination {
+        case mainTab(MainTabFeature)
+        case registerDevice(RegisterDeviceFeature)
+    }
+    
+    @ObservableState
+    public struct State {
+        public init() {}
+        @Presents var destination: Destination.State?
     }
 
-    public enum Action: Equatable {
+    @CasePathable
+    public enum Action {
         case onAppear
+        case deviceIdReceived(UUID)
+        case deviceIdGetFailed(DomainError)
+        case destination(PresentationAction<Destination.Action>)
     }
 
     @Dependency(\.deviceUseCases) var device
@@ -28,14 +42,28 @@ public struct AppRootFeature: Sendable {
         Reduce { state, action in
             switch action {
             case .onAppear:
-                return .run { _ in
-                    let deviceId = try await device.getDeviceId()
-                } catch: { error, _ in
-                    if let error = error as? DomainError {
-                        print(error, "🔥")
-                    }
+                return .runDomainError { send in
+                    await send(.deviceIdReceived(try await device.getDeviceId()))
+                } catch: { error, send in
+                    await send(.deviceIdGetFailed(error))
                 }
+                
+            case .deviceIdReceived:
+                // 端末IDが登録されていた場合はMainTabへ遷移させる
+                return .none
+
+            case .deviceIdGetFailed(let error):
+                print(error, "🔥")
+                if error == .notFound {
+                    // 端末IDが未登録の場合は、端末登録画面に遷移させる
+                    state.destination = .registerDevice(.init())
+                }
+                return .none
+                
+            case .destination:
+                return .none
             }
         }
+        .ifLet(\.$destination, action: \.destination)
     }
 }
