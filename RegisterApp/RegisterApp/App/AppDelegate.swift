152137cbea0fcd8f26f672a24f3aac9cb489acb6
hideto1198 <hideto1198@yahoo.co.jp>
2025-04-17 00:20:12 +0900

    デバッグ時に保存した端末IDを削除できるようにした

--- RegisterApp/RegisterApp/App/AppDelegate.swift
+++ RegisterApp/RegisterApp/App/AppDelegate.swift
@@ -11,6 +11,7 @@ import AppRootFeature
 import FirebaseCore
 import FirebaseFirestore
 import Composition
+import Infrastructure
 
 final class AppDelegate: NSObject, UIApplicationDelegate {
     let store = Store(initialState: AppRootFeature.State()) {
@@ -19,7 +20,22 @@ final class AppDelegate: NSObject, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         DeviceUseCases.serviceName = Bundle.main.bundleIdentifier ?? "default.bundle.identifier"
+        #if DEBUG
+        // デバッグ用でKeychainに保存した端末IDを削除する
+        resetKeychain(service: DeviceUseCases.serviceName)
+        #endif
         FirebaseApp.configure()
         return true
     }
 }
+
+
+#if DEBUG
+extension AppDelegate {
+    private func resetKeychain(service: String) {
+        Task {
+            await KeychainManager.shared.deleteDeviceId(service: service)
+        }
+    }
+}
+#endif
