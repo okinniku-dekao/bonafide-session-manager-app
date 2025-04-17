eb200a9e403e1f27847281aea4cea6a27aab1242
hideto1198 <hideto1198@yahoo.co.jp>
2025-04-17 17:34:05 +0900

    接続待機画面作成

---
+++ RegisterApp/RegisterApp/Sources/Presentation/StreamConnectionFeature/Sources/Feature/StreamConnectionFeature.swift
@@ -0,0 +1,23 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import ComposableArchitecture
+
+@Reducer
+public struct StreamConnectionFeature {
+    public init() {}
+    
+    @ObservableState
+    public struct State {}
+    
+    public enum Action {}
+    
+    public var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            default:
+                return .none
+            }
+        }
+    }
+}
---
+++ RegisterApp/RegisterApp/Sources/Presentation/StreamConnectionFeature/Sources/View/StreamConnectionView.swift
@@ -0,0 +1,36 @@
+//
+//  StreamConnectionView.swift
+//  StreamConnectionFeature
+//
+//  Created by 東　秀斗 on 2025/04/17.
+//
+
+import SwiftUI
+import ComposableArchitecture
+import PresentationHelper
+
+struct StreamConnectionView: View {
+    @Perception.Bindable private var store: StoreOf<StreamConnectionFeature>
+    
+    public init(store: StoreOf<StreamConnectionFeature>) {
+        self.store = store
+    }
+    
+    var body: some View {
+        VStack {
+            ProgressView()
+        }
+        .trackingPerception()
+        .navigationTitle("接続待機")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    StreamConnectionView(
+        store: Store(
+            initialState: StreamConnectionFeature.State(),
+            reducer: { StreamConnectionFeature() }
+        )
+    )
+}
