e9ba92f65009e4c97f5faee7b9629c06021ee5f6
hideto1198 <hideto1198@yahoo.co.jp>
2025-04-17 20:06:43 +0900

    streamをcouncurrencyに変更

--- BonafideSharedFramework/Sources/PresentationHelper/Sources/Effect+Extension.swift
+++ BonafideSharedFramework/Sources/PresentationHelper/Sources/Effect+Extension.swift
@@ -10,12 +10,12 @@ import Domain
 
 public extension Effect {
     static func stream<T: Equatable & Sendable>(
-        _ stream: AsyncThrowingStream<T, any Error>,
+        _ stream: @escaping @Sendable () async throws -> AsyncThrowingStream<T, any Error>,
         map action: @escaping @Sendable (T) -> Action,
         catch handler: (@Sendable (_ error: any Error, _ send: Send<Action>) async -> Void)? = nil
     ) -> Effect {
         .run { [action = action] send in
-            for try await value in stream {
+            for try await value in try await stream() {
                 await send(action(value))
             }
         } catch: {
