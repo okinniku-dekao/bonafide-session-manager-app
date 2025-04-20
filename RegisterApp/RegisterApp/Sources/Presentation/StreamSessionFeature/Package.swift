86b5cd8d7859b914d8be5aa7c44c946ac64f51bd
hideto1198 <hideto1198@yahoo.co.jp>
2025-04-20 10:57:43 +0900

    Lottie追加

--- RegisterApp/RegisterApp/Sources/Presentation/StreamSessionFeature/Package.swift
+++ RegisterApp/RegisterApp/Sources/Presentation/StreamSessionFeature/Package.swift
@@ -13,6 +13,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.18.0"),
+        .package(url: "https://github.com/airbnb/lottie-spm.git", from: "4.5.1"),
         .package(path: "../../../../../BonafideSharedFramework"),
         .package(path: "../AppSharedState")
     ],
@@ -21,6 +22,7 @@ let package = Package(
             name: "StreamSessionFeature",
             dependencies: [
                 .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
+                .product(name: "Lottie", package: "lottie-spm"),
                 .product(name: "Composition", package: "BonafideSharedFramework"),
                 .product(name: "Domain", package: "BonafideSharedFramework"),
                 .product(name: "PresentationHelper", package: "BonafideSharedFramework"),
