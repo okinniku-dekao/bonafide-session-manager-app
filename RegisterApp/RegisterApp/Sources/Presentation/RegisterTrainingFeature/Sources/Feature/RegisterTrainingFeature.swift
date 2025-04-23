2d534ace0d6cbd396b88bc8610c0eddb946c6556
hideto1198 <hideto1198@yahoo.co.jp>
2025-04-23 14:56:04 +0900

    TCAの初期設定

--- RegisterApp/RegisterApp/Sources/Presentation/RegisterTrainingFeature/Sources/Feature/RegisterTrainingFeature.swift
+++ RegisterApp/RegisterApp/Sources/Presentation/RegisterTrainingFeature/Sources/Feature/RegisterTrainingFeature.swift
@@ -1,2 +1,21 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import ComposableArchitecture
+
+@Reducer
+public struct RegisterTrainingFeature {
+    public init() {}
+    
+    public struct State {
+        public init() {}
+    }
+    
+    public enum Action {}
+    
+    public var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            return .none
+        }
+    }
+}
--- RegisterApp/RegisterApp/Sources/Presentation/RegisterTrainingFeature/Sources/View/RegisterTrainingView.swift
+++ RegisterApp/RegisterApp/Sources/Presentation/RegisterTrainingFeature/Sources/View/RegisterTrainingView.swift
@@ -1,18 +1,30 @@
 //
-//  SwiftUIView.swift
+//  RegisterTrainingView.swift
 //  RegisterTrainingFeature
 //
 //  Created by 東　秀斗 on 2025/04/23.
 //
 
 import SwiftUI
+import ComposableArchitecture
 
-struct SwiftUIView: View {
-    var body: some View {
+public struct RegisterTrainingView: View {
+    @Perception.Bindable private var store: StoreOf<RegisterTrainingFeature>
+    
+    public init(store: StoreOf<RegisterTrainingFeature>) {
+        self.store = store
+    }
+    
+    public var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    SwiftUIView()
+    RegisterTrainingView(
+        store: Store(
+            initialState: RegisterTrainingFeature.State(),
+            reducer: { RegisterTrainingFeature() }
+        )
+    )
 }
